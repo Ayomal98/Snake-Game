065946b0c9e2335c53c17393dec139d229ddd91e
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-31 00:09:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("canvas");
+const cxs = canvas.getContext("2d");
+cxs.beginPath();
+cxs.fillStyle = "red";
+cxs.fillRect = (20, 20, 100, 150);
+// //load Images
+// let imageName = new Image();
+// imageName.src = "img/apple.jpg";
+
+// //load the audio-clip
+// let audioName = new Audio();
+// audioName.src;
+
+// //draw the image
+// cxs.drawimage(imageName, 40, 50, 25, 25);
+
+// //draw the rectangle
+// cxs.fillStyle = "red";
+// cxs.fillRect = (100, 300, 30, 30);
