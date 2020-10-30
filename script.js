5d58e0c3c7cc188b0f6d50e350b3ead7873bd8a6
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-31 00:57:20 +0530

    added the food material

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ const canvas = document.getElementById("snake");
 const cxs = canvas.getContext("2d");
 let box = 32; //size of the unit
 
-let snake = [];
-snake[0] = { x: 9 * box, y: 10 * box };
 //load Images
 let imageFood = new Image();
 imageFood.src = "img/apple.jpg";
 
 let imageGround = new Image();
 imageGround.src = "img/ground.jpg";
-//load the audio-clip
-let audioName = new Audio();
-audioName.src;
 
-//draw the image
-cxs.drawimage(imageName, 40, 50, 25, 25);
+//create the snake
+let snake = [];
+snake[0] = {
+  x: 9 * box,
+  y: 10 * box,
+};
 
-//draw the rectangle
-cxs.fillStyle = "red";
-cxs.fillRect = (100, 300, 30, 30);
+//create the food
+let food = {
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 17 + 1) * box,
+};
+
+//create the score
+let score = 0;
+
+//drawing the details into the canvas
+function draw() {
+  cxs.drawImage(imageGround, 0, 0);
+}
+
+let game = setInterval(draw, 100);
